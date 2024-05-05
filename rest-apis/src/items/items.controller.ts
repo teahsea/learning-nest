db2ee4e86ad6d627238b82af7e2cc248494f695a
Nikunj Prajapati <nikunjprajapati0016@gmail.com>
2024-05-05 10:36:56 +0530

    Added basic items routes

--- rest-apis/src/items/items.controller.ts
+++ rest-apis/src/items/items.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Body } from '@nestjs/common';
+import { CreateItemDto } from './dto/create-item.dto';
 
 @Controller('items')
-export class ItemsController {}
+export class ItemsController {
+
+    @Get()
+    findAll(): string {
+        return 'Find all items!';
+    }
+
+    @Post()
+    create(@Body() createItemDto: CreateItemDto): string {
+        return `name: ${createItemDto.name}, desc: ${createItemDto.description}, qty: ${createItemDto.qty}`;
+    }
+
+    @Put()
+    update(): string {
+        return 'Updated item!';
+    }
+
+    @Delete()
+    remove(): string {
+        return 'Removed item!';
+    }
+ 
+}
