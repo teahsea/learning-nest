b33abcf943e3e7342307bb98b44c5a09c90f462c
Nikunj Prajapati <nikunjprajapati0016@gmail.com>
2024-05-05 11:03:52 +0530

    Implements Items Service in controller

--- rest-apis/src/items/items.controller.ts
+++ rest-apis/src/items/items.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Get, Post, Put, Delete, Body, Param } from '@nestjs/common';
 import { CreateItemDto } from './dto/create-item.dto';
+import { ItemsService } from './items.service';
+import { Item } from './interfaces/item.interface';
 
 @Controller('items')
 export class ItemsController {
+    constructor(private readonly itemsService: ItemsService) {}
 
     @Get()
-    findAll(): string {
-        return 'Find all items!';
+    findAll(): Item[] {
+        return this.itemsService.findAll();
     }
 
     @Get(':id')
--- rest-apis/src/items/items.service.ts
+++ rest-apis/src/items/items.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { Item } from './interfaces/item.interface';
 
 @Injectable()
-export class ItemsService {}
+export class ItemsService {
+    private readonly items: Item[] = [
+        {
+            id: "asfdsfsE234",
+            name: "item 1",
+            qty: 100,
+            description: "This is dummy description"
+        },
+        {
+            id: "asfdsfsE234",
+            name: "item 2",
+            qty: 90,
+            description: "This is dummy description"
+        },
+        {
+            id: "asfdsfsE234",
+            name: "item 3",
+            qty: 1000,
+            description: "This is dummy description"
+        }
+    ];
+
+    findAll(): Item[] {
+        return this.items;
+    }
+}
